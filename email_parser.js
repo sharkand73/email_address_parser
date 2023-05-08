2b187cf87547b615a9410464261aa044357d71b7
Andrew Sharkey <sharkand@hotmail.com>
2023-05-08 22:58:01 +0100

    Email address parsing works

--- email_parser.js
+++ email_parser.js
@@ -1,25 +1,33 @@
 const fs =require('fs');
 
+const fileName = './email_addresses.txt';
+fs.readFile(fileName, 'utf-8', (err, data) => callBack(err, data));
+
+
+
+
+// Methods
 
 const callBack = (err, data) => {
     if (err) {
         console.error(err);
         return;
       }
-      processEmails(data);
+      const emailArray = processEmails(data);
+      console.log(emailArray);
+      console.log(emailArray.length);
 }
-
-const fileName = './email_addresses.txt';
-fs.readFile(fileName, 'utf-8', (err, data) => callBack(err, data));
 
 const processEmails = (emails) => {
     let contactArray = emails.split(', ');
-    let emailArray = contactArray.map(contact => getEmail(contact)
+    let emailArray = contactArray.map(contact => 
+        getEmailFromContact(contact)
     );
     emailArray.sort((a,b) => a.toLowerCase()>b.toLowerCase() ? 1 : -1);
-    console.log(emailArray);
+    return emailArray;
 }
-const getEmail = contact => {
+
+const getEmailFromContact = contact => {
     let index1 = contact.indexOf('<');
     let index2 = contact.indexOf('>');
     return contact.slice(index1 + 1, index2);
